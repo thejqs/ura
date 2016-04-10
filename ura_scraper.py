2f829b1dfa921b32db3480249c957d177b243f21
Jacob Sanders <j.q.sanders@gmail.com>
2016-04-10 14:03:29 -0400

    functionized the mini-scraper

--- ura_scraper.py
+++ ura_scraper.py
@@ -8,24 +8,68 @@ from lxml.cssselect import CSSSelector
 import StringIO
 import time
 
-driver = webdriver.Chrome()
-driver.get('https://public-cpgh.epropertyplus.com/landmgmtpub/app/base/propertySearch?searchInfo=%7B%22criteria%22%3A%7B%22criterias%22%3A[]%7D%7D#')
 
-time.sleep(1)
+def open_url(url):
+    '''
+    navigates to our starting point
+    '''
+    driver = webdriver.Chrome()
+    driver.get()
 
-driver.find_element_by_css_selector('div.modal-footer > button').click()
 
-time.sleep(1)
+def ditch_popup():
+    '''
+    clicks out of an annoying modal that shouldn't be there in the first place
+    '''
+    time.sleep(1)
+    driver.find_element_by_css_selector('div.modal-footer > button').click()
 
-raw_txt = driver.find_element_by_css_selector('#propertyListTable')
 
-txt = raw_txt.get_attribute('innerHTML')
+def get_text():
+    '''
+    pulls out the raw text that contains what we're interested in
+    '''
+    time.sleep(1)
+    raw_txt = driver.find_element_by_css_selector('#propertyListTable')
+    return raw_txt.get_attribute('innerHTML')
 
-parser = etree.HTMLParser()
 
-tree = etree.parse(StringIO.StringIO(txt), parser)
+def parse_text(text):
+    '''
+    turns the raw text into a DOM tree
+    '''
+    parser = etree.HTMLParser()
+    return etree.parse(StringIO.StringIO(text), parser)
 
-messy_text = tree.xpath('//*/text()')
 
-with open('messy_text.txt', 'a+') as f:
-    print >> f, messy_text
+def get_target_text(tree):
+    '''
+    collects messy but useful strings from the DOM tree
+    '''
+    return tree.xpath('//*/text()')
+
+
+def write_target_text_to_file(target_text):
+    '''
+    stashes our text in a file where a collaborator can clean it
+    '''
+    with open('messy_text.txt', 'a+') as f:
+        for line in f:
+            print >> f, messy_text
+
+
+def do_the_damn_thang():
+    '''
+    runs the functions to slurp up the data thingies
+    '''
+    open_url(url)
+    ditch_popup()
+    text = get_text()
+    tree = parse_text(text)
+    target_text = get_target_text(tree)
+    write_target_text_to_file(target_text)
+
+
+if __name__ == '__main__':
+    url = 'https://public-cpgh.epropertyplus.com/landmgmtpub/app/base/propertySearch?searchInfo=%7B%22criteria%22%3A%7B%22criterias%22%3A[]%7D%7D#'
+    do_the_damn_thang()
